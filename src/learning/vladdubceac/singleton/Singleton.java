b14dc2e2684c81ad3469ee711b58bc8dbf68fd48
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-14 00:18:53 +0200

    First commit

---
+++ src/learning/vladdubceac/singleton/Singleton.java
@@ -0,0 +1,26 @@
+package learning.vladdubceac.singleton;
+
+public class Singleton {
+
+    // create an instance of singleton in a static initializer, code is guaranteed to be thread safe
+    private static Singleton uniqueInstance = new Singleton();
+
+    // an instance attribute
+    private int data = 0;
+
+    public Singleton() {
+    }
+
+    public static Singleton getInstance(){
+        // we already got an instance so just return it
+        return uniqueInstance;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData(){
+        return data;
+    }
+}
